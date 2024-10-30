bd9e3810e2cd13b26ff962c3ee82791759c5361f
Anand Patil <royalchiku14@gmail.com>
2024-10-30 14:09:00 +0530

    MidBase added to game scene Rotate renamed to RotateMidBase

--- Assets/Scripts/RotateMidBase.cs
+++ Assets/Scripts/RotateMidBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class RotateMidBase : MonoBehaviour
 {
 
     public float rotaionSpeed;
