1ba26270183bf8ed0d7fc955e941e753741a160d
Anand Patil <royalchiku14@gmail.com>
2024-10-31 05:37:41 +0530

    Needs work. Local camera rotation feature tht is not networked

--- Assets/Scripts/Multiplayer/LocalCameraManager.cs
+++ Assets/Scripts/Multiplayer/LocalCameraManager.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class LocalCameraManager : MonoBehaviour
 {
 
+    public GameObject camPos;
+
+
+    // Components
+    Camera localCam;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Detach Cam if it is enabled
+        if (gameObject.activeInHierarchy)
+        {
+            transform.parent = null;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(camPos == null)
+            return;
+            
+        if (localCam == null)
+            return;
+            
+        // Move Camera to player Pos
+        transform.position = camPos.transform.localPosition;
     }
 }
